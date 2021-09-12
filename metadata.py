46075af293e76f2c4fd31c615cdbeafc8aa3b86e
trwn <trwndoesmc@gmail.com>
2021-09-11 23:37:33 -0400

    Update metadata.py

--- metadata.py
+++ metadata.py
@@ -5,14 +5,15 @@ import json
 dirname = os.path.dirname(__file__)
 
 csv_file = open(dirname + '/data/punks.csv','r')
-csv_reader = csv.DictReader(csv_file,fieldnames = ('Name:','Background:', 'Type:', 'Mouth:', 'Accessory:', 'Eyes:', 'Hair:', 'Beard:', 'Psych DNA:'))
+csv_reader = csv.DictReader(csv_file,fieldnames = ('image:','Name:','Background:', 'Type:', 'Mouth:', 'Accessory:', 'Eyes:', 'Hair:', 'Beard:', 'Psych DNA:'))
 
 lcount = 0
 for row in csv_reader:
     out = json.dumps(row, indent=4)
 
-    jsonoutput = open(dirname + '/fin/PsychPunk'+str(lcount)+'.json','w')
+    jsonoutput = open(dirname + '/fin/PsychPunk_'+str(lcount)+'.json','w')
     jsonoutput.write(out)
     lcount+=1
+os.remove(dirname + '/fin/PsychPunk_0.json')
 jsonoutput.close()
 csv_file.close()
